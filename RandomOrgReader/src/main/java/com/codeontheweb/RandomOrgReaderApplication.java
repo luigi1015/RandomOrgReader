a5a16fecdc25dd9c5703bf2e0cb5c039126eabda
Jeff <github@314ghz.com>
2018-12-09 19:34:07 -0600

    Made some modifications for running on AWS Lambdas. Note: The zip file created from the build command seems to work, the Jar file doesn't.

--- RandomOrgReader/src/main/java/com/codeontheweb/RandomOrgReaderApplication.java
+++ RandomOrgReader/src/main/java/com/codeontheweb/RandomOrgReaderApplication.java
@@ -1,13 +1,27 @@
 package com.codeontheweb;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+
 @SpringBootApplication
-public class RandomOrgReaderApplication
+public class RandomOrgReaderApplication implements RequestStreamHandler
 {
 	public static void main(String[] args)
 	{
 		SpringApplication.run(RandomOrgReaderApplication.class, args);
+	}
+
+	@Override
+	public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException
+	{
+		String[] args = {};
+		main(args);
 	}
 }
